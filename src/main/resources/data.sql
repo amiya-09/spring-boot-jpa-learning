3b1c1bb0b6890701cb2ca041cfd9d09859b0e130
amiyasingh <23ucc513@lnmiit.ac.in>
2026-07-03 09:49:39 +0530

    Hibernate ORM, JPA, Entities and Tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO product_table
+(price, quantity, sku, title_x, created_at, updated_at)
+VALUES
+    (12.4, 4, 'parle734', 'Parle Biscuit', NOW(), NOW()),
+    (12.4, 1, 'pepsi123', 'Pepsi', NOW(), NOW()),
+    (16.6, 3, 'pepsi156', 'Mazza', NOW(), NOW()),
+    (17.8, 2, 'choco123', 'Choco', NOW(), NOW());
+
